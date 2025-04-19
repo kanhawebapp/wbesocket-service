96e41e43e406e8f4c4b196ee2d9e1d9ac098214f
Satyendra-dhwani <satyender@dhwaniastro.com>
2025-04-19 16:49:23 +0530

    initial commit

--- index.js
+++ index.js
@@ -154,6 +154,7 @@ socket.on("chat_request", (data) => {
 
   socket.on("chat_rejected_astrologer", (data) => {
 
+    console.log("Received chat_rejected_astrologer event:", data);
     if (!data.room_id) {
       console.log("Error: Room ID is missing.");
       return;
