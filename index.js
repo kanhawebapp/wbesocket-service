fbb3ba3dac1d7ebcd7f02a0ebb818678cf410ce5
tej-eng <tej@dhwaniastro.com>
2025-08-16 18:30:26 +0530

    remove port from index

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ import { verifyPayment } from "./controller/verifyPayment.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8001;
+const port = process.env.PORT;
 const server = createServer(app);
 
 const io = new Server(server, {
