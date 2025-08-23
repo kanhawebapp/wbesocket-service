78c94dd3142171b4ee0871a9fe0d50ce9cfcde9c
tej-eng <tej@dhwaniastro.com>
2025-08-23 15:47:25 +0530

    add image path

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ dhwaniNamespace.use(jwtAuthMiddleware);
 
 // Attach your socket handlers here
 socketHandler(dhwaniNamespace, pubClient, subClient);
-app.use("/uploads", express.static(process.env.UPLOADS_DIR));
+app.use("/uploads", express.static(process.env.UPLOADS_DIR || "uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
