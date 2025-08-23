4239903733cc86aa9f63d44c02b75c7d1015419a
tej-eng <tej@dhwaniastro.com>
2025-08-23 18:00:16 +0530

    upload url change

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ dhwaniNamespace.use(jwtAuthMiddleware);
 
 // Attach your socket handlers here
 socketHandler(dhwaniNamespace, pubClient, subClient);
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(process.env.UPLOADS_DIR));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
