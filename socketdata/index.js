8441b4f3dcd33f31285e7550b23030d94079e127
tejpal singh <tej@dhwaniastro.com>
2025-12-23 16:01:23 +0530

    add auto disconnect chat event

--- socketdata/index.js
+++ socketdata/index.js
@@ -240,7 +240,7 @@ async function socketHandler(io, pubClient, subClient) {
         socket.on("autodisconnect", async (data) => {
           try {
             console.log("[Socket Event] autodisconnect", data);
-            await autoChat({ roomId: data.room_id });
+           // await autoChat({ roomId: data.room_id });
             let roomId = data.room_id;
             socket.to(roomId).emit("user_disconnected", { message: "A user has left the chat.", socketId: socket.id, roomId:roomId});
             publish(pubClient, "astrologer_disconnected", { message: "Auto Disconnect Chat By Astrologer.", socketId: socket.id, roomId:roomId });
